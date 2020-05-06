80267fe7525355024006eb1a31cf9a54b90b79bc
DonPandos <ewrr_96@mail.ru>
2020-05-06 14:26:24 +0300

    first commit

---
+++ resources/views/form.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+  <form action="">
+    <label for="firstname">Имя</label>
+    <input type="text" name="firstname" id="firstname">
+
+    <label for="secondname">Фамилия</label>
+    <input type="text" name="firstname" id="firstname">
+
+    <label for="patronymic">Отчество</label>
+    <input type="text" name="patronymic" id="patronymic">
+
+    <label for="phone">Номер телефона</label>
+    <input type="text" name="phone" id="phone">
+
+    <label for="email">Email</label>
+    <input type="text" name="email" id="email">
+
+
+  </form>
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="/css/uikit.css">
+        <link rel="stylesheet" href="/css/app.css">
+
+    </head>
+    <body>
+      <a href="/form" class="uk-button uk-button-primary uk-width-1-1 uk-margin-small-bottom">Подать заявку</a>
+    </body>
+</html>
