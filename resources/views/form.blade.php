f8304047f895b6a161259ae8788a3335cb1059d8
DonPandos <ewrr_96@mail.ru>
2020-05-06 14:57:02 +0300

    debugbar, auth

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -4,26 +4,27 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="/css/uikit.css">
   <title>Document</title>
 </head>
 <body>
-  <form action="">
-    <label for="firstname">Имя</label>
-    <input type="text" name="firstname" id="firstname">
+  <form action=""  class="uk-form-horizontal uk-form-width-large">
+    <label for="firstname" class="uk-form-label">Имя</label>
+    <input type="text" name="firstname" id="firstname" placeholder="Введите имя" class="uk-input">
 
-    <label for="secondname">Фамилия</label>
-    <input type="text" name="firstname" id="firstname">
+    <label for="secondname" class="uk-form-label">Фамилия</label>
+    <input type="text" name="firstname" id="firstname" placeholder="Введите фамилию" class="uk-input">
 
-    <label for="patronymic">Отчество</label>
-    <input type="text" name="patronymic" id="patronymic">
+    <label for="patronymic" class="uk-form-label">Отчество</label>
+    <input type="text" name="patronymic" id="patronymic" placeholder="Введите отчество" class="uk-input">
 
-    <label for="phone">Номер телефона</label>
-    <input type="text" name="phone" id="phone">
+    <label for="phone" class="uk-form-label">Номер телефона</label>
+    <input type="tel" name="phone" id="phone" placeholder="Введите номер телефона" class="uk-input">
 
-    <label for="email">Email</label>
-    <input type="text" name="email" id="email">
+    <label for="email" class="uk-form-label">Email</label>
+    <input type="text" name="email" id="email" placeholder="Введите email" class="uk-input">
 
-
+    <button type="submit" name="button" class="uk-button uk-button-default">Отправить</button>
   </form>
 </body>
 </html>
